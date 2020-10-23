986571e2b201da588acf09b0ddea6e0bf69ade0e
s3fs <northfacebodybags@yahoo.com>
2020-10-24 01:47:34 +0500

    Updates

---
+++ precache-manifest.8404f72a91178fb9db84d45914e1d191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1c02f0675e5d61826451fbda76c2d3",
+    "url": "/worterbuch/index.html"
+  },
+  {
+    "revision": "118d0f8abce0061cfc6f",
+    "url": "/worterbuch/static/css/main.60f755de.chunk.css"
+  },
+  {
+    "revision": "93ed7cad4c28f7a6eb23",
+    "url": "/worterbuch/static/js/2.c8be5764.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/worterbuch/static/js/2.c8be5764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "118d0f8abce0061cfc6f",
+    "url": "/worterbuch/static/js/main.b059a6d9.chunk.js"
+  },
+  {
+    "revision": "af2ea5bcfd63b87d00c3",
+    "url": "/worterbuch/static/js/runtime-main.3b6d7386.js"
+  }
+]);
